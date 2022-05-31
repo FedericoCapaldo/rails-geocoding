c6f5531a3b1050e175444141ea037c0941493842
Federico Capaldo <federico.capaldo1@gmail.com>
2022-05-31 15:04:32 +0100

    using markers from our rails backend to show data on the map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,13 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = { 'apiKey': String };
+  static values = { 'apiKey': String, 'markers': Array };
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue;
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
     });
+    this.#addMarkersToMap();
+  }
+
+  #addMarkersToMap() {
+    console.log(this.markersValue);
+    this.markersValue.forEach((myMarker) => {
+      new mapboxgl.Marker()
+        .setLngLat([myMarker.lng, myMarker.lat])
+        .addTo(this.map);
+    });
+
   }
 }
