c729f7f8992db7b17088f2043d64dde09e3d9639
Federico Capaldo <federico.capaldo1@gmail.com>
2022-05-31 14:52:07 +0100

    actually connecting map through stimulus data

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
+  static values = { 'apiKey': String };
+
   connect() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZm9yd29ya3dob2tub3dzd2h5IiwiYSI6ImNsM3UzZGk2NjBpdm0zanJ4Z3Ezb25qdDAifQ.Nfp1e6dOAG5Xbmt9D7DJxw';
+    mapboxgl.accessToken = this.apiKeyValue;
     const map = new mapboxgl.Map({
-      container: 'map', // container ID
+      container: this.element,
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
     });
   }
 }
