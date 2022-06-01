b35fe111617d7fa1afd18952c6c569c73f8f0cf0
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-01 01:29:12 +0100

    added search bar to new flat page

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
+// Connects to data-controller="address-autocomplete"
+export default class extends Controller {
+  static values = { apiKey: String };
+  static target = ["address"];
+
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    });
+    this.geocoder.addTo(this.element);
+  }
+}
