3cd28107a93444f52ace3f5b95a73edae93e9aea
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-01 01:11:49 +0100

    added mapbox searchbar

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -12,6 +13,10 @@ export default class extends Controller {
     });
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }));
   }
 
   #fitMapToMarkers() {
