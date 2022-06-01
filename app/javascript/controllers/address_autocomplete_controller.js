eb1f8abc852728d53b7ad417a5b6a32f64ac874c
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-01 01:45:00 +0100

    correctly autocompleting address from search bar on new flat page

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -4,7 +4,7 @@ import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 // Connects to data-controller="address-autocomplete"
 export default class extends Controller {
   static values = { apiKey: String };
-  static target = ["address"];
+  static targets = ["address"];
 
   connect() {
     this.geocoder = new MapboxGeocoder({
@@ -12,5 +12,15 @@ export default class extends Controller {
       types: "country,region,place,postcode,locality,neighborhood,address"
     });
     this.geocoder.addTo(this.element);
+    this.geocoder.on("result", event => this.#setInputValue(event));
+    this.geocoder.on("clear", () => this.#clearInputValue());
+  }
+
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"];
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = "";
   }
 }
